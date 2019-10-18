09d01c1df91afcb0d069b4b37529759b7699a17e
mack <15jpsm@queensu.ca>
2019-10-18 12:40:14 -0400

    Added session class and methods (not complete)

--- classes.py
+++ classes.py
@@ -21,3 +21,24 @@ class user:
 
     def setPassword(self, pw):
         self.password = pw
+
+
+class session:
+    def __init__(self, uid, sessionType, timeSignedIn):
+        self.uid = uid
+        self.sesionType = sessionType
+        self.timeSignedIn = timeSignedIn
+
+    def login(self, username, password, sType):
+        # If username + password matches record in db
+        # return true else false
+        if username:  # (pseudo) need to verify if user details match
+            self.sessionType = sType
+            return True
+        else:
+            return False
+
+    def logout(self):
+        self.uid = None
+        self.sessionType = None
+        self.timeSignedIn = None
