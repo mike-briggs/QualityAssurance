d15fc757f72404b1918c8bab1f49879742cb64a7
mack <15jpsm@queensu.ca>
2019-10-18 12:27:31 -0400

    User class defined

--- classes.py
+++ classes.py
@@ -1,10 +1,23 @@
 class user:
-	def __init__(self, username, password, fname, lname):
-		self.username = username
-		self.password = password
-		self.fname	  = fname
-		self.lname	  = lname
-	
-	def getUser(self):
-		print(self.fname+" "+self.lname);
-		
+    def __init__(self, userid, username, password, fname, lname):
+        self.userid = userid
+        self.username = username
+        self.password = password
+        self.fname = fname
+        self.lname = lname
+
+    def getName(self):
+        print(self.fname+" "+self.lname)
+
+    def setName(self, first, last):
+        self.fname = first
+        self.lname = last
+
+    def setUsername(self, un):
+        self.username = un
+
+    def getUsername(self):
+        print(self.username)
+
+    def setPassword(self, pw):
+        self.password = pw
