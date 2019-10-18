413c31b250a0b1b7de8ede55526c6a650bb55d59
mack <15jpsm@queensu.ca>
2019-10-18 14:11:35 -0400

    inAccounts populated

--- inAccounts.py
+++ inAccounts.py
@@ -1,2 +1,19 @@
-msg = "Hello World"
-print(msg)
+import json
+import classes
+
+accountsFile = open("your_accounts_list.txt", "r")
+num_lines_in_file = sum(1 for line in open('your_accounts_list.txt'))
+
+for i in num_lines_in_file:
+    line = accountsFile.readline()
+    delimited = line.split(" ")
+    accountNumber = delimited[0]
+    accountBalance = delimited[1]
+    accountName = delimited[2]
+
+    masterAccountsList = []
+    masterAccountsList.append(
+        account(accountNumber, accountName, accountBalance))
+
+
+accountsFile.close()
