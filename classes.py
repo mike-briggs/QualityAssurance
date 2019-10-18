9ce29259e2e8518e402cedd15b8d0056571531ac
mack <15jpsm@queensu.ca>
2019-10-18 13:31:59 -0400

    update classes

--- classes.py
+++ classes.py
@@ -1,28 +1,3 @@
-class user:
-    def __init__(self, userid, username, password, fname, lname):
-        self.userid = userid
-        self.username = username
-        self.password = password
-        self.fname = fname
-        self.lname = lname
-
-    def getName(self):
-        print(self.fname+" "+self.lname)
-
-    def setName(self, first, last):
-        self.fname = first
-        self.lname = last
-
-    def setUsername(self, un):
-        self.username = un
-
-    def getUsername(self):
-        print(self.username)
-
-    def setPassword(self, pw):
-        self.password = pw
-
-
 class session:
     def __init__(self, uid, sessionType, timeSignedIn):
         self.uid = uid
@@ -42,3 +17,34 @@ class session:
         self.uid = None
         self.sessionType = None
         self.timeSignedIn = None
+
+
+class account:
+    def __init__(self, accountNumber, accountName, password, fname, lname):
+        self.accountNumber = accountNumber
+        self.accountName = accountName
+        self.password = password
+        self.fname = fname
+        self.lname = lname
+
+    def setAccountNumber(self, accNumber):
+        self.accountNumber = accNumber
+
+    def setAccountName(self, accName):
+        self.accountName = accName
+
+    def getName(self):
+        print(self.fname+" "+self.lname)
+
+    def setName(self, first, last):
+        self.fname = first
+        self.lname = last
+
+    def setUsername(self, un):
+        self.username = un
+
+    def getUsername(self):
+        print(self.username)
+
+    def setPassword(self, pw):
+        self.password = pw
