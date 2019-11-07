2dab4df7c6dfac19a32168601598dca92186f900
mack <15jpsm@queensu.ca>
2019-11-06 19:27:53 -0500

    Transfer working

--- src/frontend.py
+++ src/frontend.py
@@ -3,7 +3,7 @@ import sys
 import json
 
 
-def logout(line, outputFile):
+def logout(outputFile):
     with open(outputFile, 'a') as wf:
         wf.write('\nEOS 0000000 000 0000000 ***')
     return 0
@@ -12,7 +12,7 @@ def logout(line, outputFile):
 # -1    - error/not logged in
 
 
-def login(line, outputFile):
+def login(outputFile):
     arg2 = input('Enter mode: ')
     print(arg2)
 
@@ -28,7 +28,7 @@ def login(line, outputFile):
         return -1
 
 
-def deposit(line, outputFile, validAccounts):
+def deposit(outputFile, validAccounts):
 
     # Validation cases
     acctNum = input("Enter account number: ")  # account num
@@ -50,14 +50,48 @@ def deposit(line, outputFile, validAccounts):
             print("Invalid amount.")
             return False
 
-        # elif acctNum not in listOfAccounts:
-        # print("Deposit account does not sexist.")
-
         # if not amount.isdigit():
         #     print("Amount is not a valid amount.")
         # elif (login == 1 and amount > 1000) or (login == 2 and amount > 999999.99):
         #     print("Over deposit limit.")
         # else:
+
+
+def transfer(outputFile, validAccounts):
+    fromAccount = input("Enter (from) account: ")
+    print(fromAccount)
+
+    # Validation cases
+    # If accout number not proper, error
+    if len(fromAccount) != 7 or not fromAccount.isdigit() or fromAccount not in validAccounts:
+        print("Invalid account number.")
+    else:
+        toAccount = input("Enter (to) account: ")
+        print(toAccount)
+        # If accout number not proper, error
+        if len(toAccount) != 7 or not toAccount.isdigit() or toAccount not in validAccounts or toAccount == fromAccount:
+            print("Invalid account number.")
+        else:
+            amount = input("Enter amount: ")
+            print(amount)
+
+            if (amount.isdigit()):                          # If amount not proper, error
+                print("Funds successfully transfered.")
+                with open(outputFile, 'a') as wf:
+                    wf.write('\nXFR '+toAccount+' ' +
+                             amount+' '+fromAccount+' ***')
+            else:
+                print("Invalid amount.")
+
+    # elif (login == 1 and amount > 1000) or (login == 2 and amount > 999999.99):  # Enforce limit
+    #     print("Over withdrawal limit.")
+
+    # elif login == 1 and amount > 5000:                  # Daily limit: TODO
+    #     print("Amount is not a valid amount.")
+
+    # else
+
+    return True
 
 # main
 
@@ -77,19 +111,19 @@ with open(outputFilepath, 'w') as wf:
     wf.write('')
 
 if(userInput == 'login'):
-    loginStatus = login(userInput, outputFilepath)
+    loginStatus = login(outputFilepath)
     if(loginStatus > 0):    # if logged in
         while(loginStatus > 0):
             userInput = input("Enter action: ")
             print(userInput)
             if userInput == "logout":
-                loginStatus = logout(userInput, outputFilepath)
+                loginStatus = logout(outputFilepath)
             elif userInput == "deposit":
-                deposit(userInput, outputFilepath, validAccounts)
+                deposit(outputFilepath, validAccounts)
             # elif userInput == "withdraw":
             #     withdraw(current, accounts, loginState, outputFile)
-            # elif userInput == "transfer":
-            #     transfer(current, accounts, loginState, outputFile)
+            elif userInput == "transfer":
+                transfer(outputFilepath, validAccounts)
             # elif userInput == "createacct":
             #     accounts = createacct(current, accounts, loginState, outputFile)
             # elif userInput == "deleteacct":
@@ -126,42 +160,6 @@ elif(userInput == 'logout'):
 #         print("Amount is not a valid amount.")
 
 #     print("WDR "+acctNum+" "+amount+" 0000000 name")
-
-#     return True
-
-
-# def transfer(transaction, listOfAccounts, login, outputFile):
-
-#     file = open(outputFile, "w+")
-#     delimited = transaction.split(" ")  # to from balance
-#     toAccount = delimited[1]
-#     fromAccount = delimited[2]
-#     amount = delimited[3]
-
-#     # Validation cases
-#     if login == 0:              # If not logged in, error
-#         print("You are not logged in.")
-
-#     elif len(toAccount) != 7 or not toAccount.isdigit():    # If accout number not proper, error
-#         print("Invalid account number.")
-
-#     elif len(fromAccount) != 7 or not fromAccount.isdigit():    # If accout number not proper, error
-#         print("Invalid account number.")
-
-#     elif toAccount not in listOfAccounts or fromAccount not in listOfAccounts:   # Make sure account exists
-#         print("Account does not exist.")
-
-#     elif not amount.isdigit():                          # If amount not proper, error
-#         print("Amount is not a valid amount.")
-
-#     elif (login == 1 and amount > 1000) or (login == 2 and amount > 999999.99):  # Enforce limit
-#         print("Over withdrawal limit.")
-
-#     elif login == 1 and amount > 5000:                  # Daily limit: TODO
-#         print("Amount is not a valid amount.")
-
-#     else
-#     print("XFR "+toAccount+" "+amount+" "+fromAccount+" name")
 
 #     return True
 
