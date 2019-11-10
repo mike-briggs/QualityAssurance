de8e90c0d1e04ba359c417e5f1e7ca17b8d96e55
connorcrowe <connor.haz.crowe@gmail.com>
2019-11-10 15:43:16 -0500

    Created basic functions, still needs some debugging

--- src/backend.py
+++ src/backend.py
@@ -1,6 +1,7 @@
 # backend.py
 # handles all (merged) transactions once a day
 import sys
+import os
 
 # Inputs:
 #   - Transaction summary file (merged from several)
@@ -27,73 +28,82 @@ import sys
 #   Create new valid accounts list from Master
 #   Output valid accounts and master accounts
 
-# Define in and output paths
-inMasterAccountListPath = sys.argv[1]
-inTransactionListPath = sys.argv[2]
-outMasterAccountListPath = sys.argv[3]
-outValidAccountListPath = sys.argv[4]
-
-# Define working variables
-MasterAccountList = [];
-ValidAccountList = [];
-TransactionList = [];
-
+# Used to help store the master accounts list
 class Account(self, accountNumber, balance, name):
     self.accountNumber = accountNumber
     self.balance = balance
     self.name = name
 
+# returns a list of account objects
 def parseMasterAccounts(filepath):
     masterAccountList = []
     with open(filepath) as f:
         masterAccountList = f.readlines()
         # stores each line (account number) in list
         for i in masterAccountList:
-            line = masterAccountList[i].split()
-            masterAccountSList[i] = new Account(line[0].strip(), line[1].split(), line[2].split())
+            line = masterAccountList[i].split(" ")
+            masterAccountList[i] = new Account(line[0].strip(), line[1].strip(), line[2].strip())
 
     # return list of valid accounts
     return masterAccountList
 
-# returns list (array) of valid accounts
-def parseValidAccounts(filepath):
-    validAccountList = []
-    with open(filepath) as f:
-        validAccountList = f.readlines()
-        # stores each line (account number) in list
-        validAccountList = [x.strip() for x in validAccountList]
+# Deposit Money into an account
+def deposit(accountList, accountNumber, amount):
+    accountList[accountList.index(accountNumber)].balance += amount
 
-    # return list of valid accounts
-    return validAccountList
+# Withdraw money from an account
+# TODO: make sure account has enough Money
+# figure out if we have to enforce daily limits
+def withdraw(accountList, accountNumber, amount):
+    accountList[accountList.index(accountNumber)].balance -= amount
 
-def deposit(accountNumber, amount)
+# Transfer money from on account to another
+# TODO: make sure account has enough money
+def transfer(accountList, toAccountNumber, amount, fromAccountNumber):
+    accountList[accountList.index(toAccountNumber)].balance += amount
+    accountList[accountList.index(fromAccountNumber)].balance += amount
 
-def withdraw(accountNumber, amount)
+# Create a new account
+# TODO: Make sure accountnumber is unique
+def createacct(accountList, accountNumber, accountName):
+    accountList.append(new Account(accountNumber, 0, accountName))
 
-def transfer(toAccountNumber, amount, fromAccountNumber)
+# Delete an account
+def deleteacct(accountList, accountNumber, accountName):
+    accountList.remove(accountNumber)
 
-def createacct(accountNumber, accountName)
+#
+# MAIN
+#
+# Define input and output file paths
+inMasterAccountListPath = sys.argv[1]
+inTransactionListPath = sys.argv[2]
+outMasterAccountListPath = sys.argv[3]
+outValidAccountListPath = sys.argv[4]
 
-def deleteacct(accountNumber, accountName)
+# Define working variables
+MasterAccountList = parseMasterAccounts(inMasterAccountListPath) # master list of accounts and balances
+ValidAccountList = []
+TransactionList = []
 
-# Main
 # Iterate through all Transactions
-for (int i=0; i < len(TransactionList); i++)
+for i in TransactionList
+    # Split each transaction into its arguments
     current = TransactionList[i].split(" ")
     # 000 1111111 222 3333333 4444
     # TYP accntTo amt accntFr name
     if current[0] == "DEP"      # Deposit
-        deposit(current[1], current[2])
+        deposit(MasterAccountList, current[1], current[2])
 
     elif current[0] == "WDR"    # Withdraw
-        withdraw(current[1], current[2])
+        withdraw(MasterAccountList, current[1], current[2])
 
     elif current[0] == "XFR"    # Transfer
-        transfer(current[1], current[2], current[3])
+        transfer(MasterAccountList, current[1], current[2], current[3])
 
     elif current[0] == "NEW"    # Create account
-        createacct(current[1], current[4])
+        createacct(MasterAccountList, current[1], current[4])
 
     elif current[0] == "DEL"    # Delete account
-        deleteacct(current[1], current[4])
+        deleteacct(MasterAccountList, current[1], current[4])
 
