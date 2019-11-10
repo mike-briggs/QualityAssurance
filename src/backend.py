3474640aef51db38fed048be8710b76eb64295bd
connorcrowe <connor.haz.crowe@gmail.com>
2019-11-10 14:41:26 -0500

    Started pseudo coding

--- src/backend.py
+++ src/backend.py
@@ -1,2 +1,10 @@
 # backend.py
 # handles all (merged) transactions once a day
+
+
+# Inputs:
+#   - Transaction summary file (merged from several)
+#   - Previous instance of Master Account List
+# Outputs:
+#   - New instance of Master Account List
+#   - New Valid Accunts List for next set of frontends
