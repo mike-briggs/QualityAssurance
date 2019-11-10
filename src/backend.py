d78d9ce2d9f5305b2853c26bccce37fa1f28edd4
connorcrowe <connor.haz.crowe@gmail.com>
2019-11-10 14:48:26 -0500

    Description writing, start teletype

--- src/backend.py
+++ src/backend.py
@@ -8,3 +8,10 @@
 # Outputs:
 #   - New instance of Master Account List
 #   - New Valid Accunts List for next set of frontends
+
+
+# Backend
+#   Read in previous Master Account list and store in local data structure
+#   Format coming in: AAAAAAA MMMMM NNNN
+#                     acctnum money name
+#   Read in transaction summary file and store in local data structure
