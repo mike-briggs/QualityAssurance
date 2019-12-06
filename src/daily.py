0ff087689765eaff528868b0d2eaea4204dedbd4
mack <15jpsm@queensu.ca>
2019-12-06 15:47:11 -0500

    reformat

--- src/daily.py
+++ src/daily.py
@@ -1,22 +1,25 @@
 import subprocess
 import os
 
-currentInputLines = ''
-            with open(inputFileName, 'r') as inputFile:
-                currentInputLines = inputFile.read()
+inputFileName = ""
+currentCommandToRun = ""
 
-            # Run the frontend script and get the output
-            # If it takes longer than a set timeout value to complete, kill it
-            frontendOutput = ''
-            try:
-                frontendProcess = subprocess.run(
-                    currentCommandToRun, 
-                    stdout=subprocess.PIPE, 
-                    stderr=subprocess.DEVNULL, 
-                    input=currentInputLines, 
-                    universal_newlines=True, 
-                    timeout=PROCESS_TIMEOUT
-                )
-                frontendOutput = frontendProcess.stdout
-            except subprocess.TimeoutExpired:
-                pass
+currentInputLines = ''
+with open(inputFileName, 'r') as inputFile:
+    currentInputLines = inputFile.read()
+
+# Run the frontend script and get the output
+# If it takes longer than a set timeout value to complete, kill it
+frontendOutput = ''
+try:
+    frontendProcess = subprocess.run(
+        currentCommandToRun,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        input=currentInputLines,
+        universal_newlines=True,
+        timeout=PROCESS_TIMEOUT
+    )
+    frontendOutput = frontendProcess.stdout
+except subprocess.TimeoutExpired:
+    pass
