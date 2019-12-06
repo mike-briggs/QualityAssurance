41fdbbb08e6f00a2b8e66a846ac36a62dfdd35a6
mike-briggs <michael.briggs@queensu.ca>
2019-12-06 15:30:37 -0500

    call frontend

--- src/daily.py
+++ src/daily.py
@@ -1,2 +1,22 @@
 import subprocess
 import os
+
+currentInputLines = ''
+            with open(inputFileName, 'r') as inputFile:
+                currentInputLines = inputFile.read()
+
+            # Run the frontend script and get the output
+            # If it takes longer than a set timeout value to complete, kill it
+            frontendOutput = ''
+            try:
+                frontendProcess = subprocess.run(
+                    currentCommandToRun, 
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.DEVNULL, 
+                    input=currentInputLines, 
+                    universal_newlines=True, 
+                    timeout=PROCESS_TIMEOUT
+                )
+                frontendOutput = frontendProcess.stdout
+            except subprocess.TimeoutExpired:
+                pass
